4b5e925b57ca5cbc2cf5c5ae067c3a12f8c51d49
kimmi1206 <kheinerimm1206@gmail.com>
2022-08-04 13:34:40 -0500

    Spring Flight Reservation REST API

---
+++ src/test/java/com/bharath/flightreservation/FlightreservationApplicationTests.java
@@ -0,0 +1,51 @@
+package com.bharath.flightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bharath.flightreservation.entities.Reservation;
+import com.bharath.flightreservation.repos.FlightRepository;
+import com.bharath.flightreservation.repos.PassengerRepository;
+import com.bharath.flightreservation.repos.ReservationRepository;
+import com.bharath.flightreservation.util.EmailUtil;
+import com.bharath.flightreservation.util.PDFGenerator;
+
+@SpringBootTest
+public class FlightreservationApplicationTests {
+
+	@Autowired
+	PassengerRepository repo;
+
+	@Autowired
+	FlightRepository flightRepo;
+
+	@Autowired
+	ReservationRepository reservationRepo;
+
+	@Autowired
+	PDFGenerator pdfGenerator;
+
+	@Autowired
+	EmailUtil emailUtil;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testGenerateItenarary() {
+		Reservation reservation = reservationRepo.findById(1L).get();
+		String filePath = "/Users/bharaththippireddy/Documents/reservations/reservation" + reservation.getId() + ".pdf";
+		pdfGenerator.generateItinerary(reservation, filePath);
+	}
+
+	@Test
+	public void testEmail() {
+		Reservation reservation = reservationRepo.findById(1L).get();
+		String filePath = "/Users/bharaththippireddy/Documents/reservations/reservation" + reservation.getId() + ".pdf";
+		emailUtil.sendItinerary("thippireddy.bharath@gmail.com", filePath);
+	}
+
+}
